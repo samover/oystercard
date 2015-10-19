3178013ef0d25d6c1367f74e6bd10c73c8b08e72
Deon Tan <deonjtan@gmail.com>
2015-10-19 15:52:09 +0100

    Top up method added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,11 @@
 class Oystercard
+  attr_reader :balance
+  DEFAULT_BALANCE = 0
+  def initialize (balance=DEFAULT_BALANCE)
+    @balance = balance
+  end
 
+  def top_up(num)
+    @balance += num
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,15 @@
 require 'oystercard'
 
 describe Oystercard do
+  it "checks that default balance is zero" do
+    subject.balance()
+    expect(subject.balance).to eq Oystercard::DEFAULT_BALANCE
+  end
 
+  describe "#top_up" do
+    it "allows for balance to be topped up" do
+      2.times { subject.top_up(10) }
+      expect(subject.balance).to eq 20
+    end
+  end
 end
