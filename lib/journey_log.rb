90d41105693bd193cd52eb1c1503ff880566c12a
samover <samuel.overloop@gmail.com>
2015-10-23 10:27:19 +0100

    all tests green

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,3 +1,19 @@
 class JourneyLog
+  def initialize(journey_klass: )
+    @journey_klass = journey_klass
+    @journeys = []
+  end
+
+  def start_journey
+  end
+
+  def current_journey
+  end
+
+  def exit_journey
+  end
+
+  def outstanding_charges
+  end
 
 end
